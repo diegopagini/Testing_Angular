6e53d2f274f3f85dda4e921feb8eb66384f676ae
Diego Pagini <diego.pagini@met.ey.com>
2021-11-09 17:57:17 +0100

    async tests

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -2,54 +2,58 @@ import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
 import { of } from "rxjs";
 import { delay } from "rxjs/operators";
 
+// Async Testing Examples
 describe("Async Testing Examples", () => {
+  // With doneFn callback we can use it to assert the result of the async operation
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
-
+    expect(test).toBe(false);
+    // Not a good practice to use setTimeout in tests
     setTimeout(() => {
-      console.log("running assertions");
-
       test = true;
-
-      expect(test).toBeTruthy();
-
+      expect(test).toBe(true);
       done();
     }, 1000);
   });
 
+  // With fakeAsync we can use tick() to advance the async operation
   it("Asynchronous test example - setTimeout()", fakeAsync(() => {
     let test = false;
-
-    setTimeout(() => {});
+    expect(test).toBe(false);
 
     setTimeout(() => {
-      console.log("running assertions setTimeout()");
-
       test = true;
+      expect(test).toBe(true);
     }, 1000);
 
-    flush();
+    // tick(1000) will advance the async operation by 1000ms and can be used to assert the result only in a fakeAsync test
+    tick(1000);
 
-    expect(test).toBeTruthy();
+    setTimeout(() => {
+      test = false;
+      expect(test).toBe(false);
+    }, 2000);
+
+    // flush() will advance the async operation by the remaining time and can be used to assert the result only in a fakeAsync test
+    flush();
   }));
 
   it("Asynchronous test example - plain Promise", fakeAsync(() => {
-    let test = false;
-
-    console.log("Creating promise");
+    let test: boolean = false;
+    console.log("Creating Promise");
 
     Promise.resolve()
       .then(() => {
-        console.log("Promise first then() evaluated successfully");
+        console.log("Promise first then() evaluated");
 
+        test = true;
         return Promise.resolve();
       })
       .then(() => {
-        console.log("Promise second then() evaluated successfully");
-
-        test = true;
+        console.log("Promise second then() evaluated");
       });
 
+    // flushMicrotask() will flush the microtask queue and can be used to assert the result only in a fakeAsync test
     flushMicrotasks();
 
     console.log("Running test assertions");
@@ -58,8 +62,7 @@ describe("Async Testing Examples", () => {
   }));
 
   it("Asynchronous test example - Promises + setTimeout()", fakeAsync(() => {
-    let counter = 0;
-
+    let counter: number = 0;
     Promise.resolve().then(() => {
       counter += 10;
 
@@ -69,11 +72,11 @@ describe("Async Testing Examples", () => {
     });
 
     expect(counter).toBe(0);
-
+    // flushMicrotasks to finish promises
     flushMicrotasks();
 
     expect(counter).toBe(10);
-
+    // tick to finish setTimeout
     tick(500);
 
     expect(counter).toBe(10);
@@ -83,20 +86,18 @@ describe("Async Testing Examples", () => {
     expect(counter).toBe(11);
   }));
 
+  // TESTING OBSERVABLES
   it("Asynchronous test example - Observables", fakeAsync(() => {
-    let test = false;
-
+    let test: boolean = false;
     console.log("Creating Observable");
-
-    const test$ = of(test).pipe(delay(1000));
+    // creating an observable that emits a value after 1 second
+    const test$ = of(true).pipe(delay(1000));
 
     test$.subscribe(() => {
       test = true;
     });
 
     tick(1000);
-
-    console.log("Running test assertions");
 
     expect(test).toBe(true);
   }));
